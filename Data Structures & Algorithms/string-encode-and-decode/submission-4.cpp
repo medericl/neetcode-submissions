c8c853f110d3b80087898cbe77144d98cad73763
Mederic <59843750+medericl@users.noreply.github.com>
2026-09-25 23:59:36 +0200

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+
+    void push_nb(string &str, string &res)
+    {
+        string nb = to_string(str.size());
+        for (const char &c : nb)
+            res.push_back(c);
+        res.push_back('#');
+    }
+
+    string encode(vector<string>& strs) {
+        string res = "";
+        for (string &str : strs)
+        {
+            push_nb(str, res);
+            for (char &c : str)
+            {
+                res.push_back(c);
+            }
+        }
+        return res;
+    }
+
+
+    vector<string> decode(string s) {
+        std::vector<string> res;
+        int i = 0;
+        while (i < s.size())
+        {
+            std::string str;
+            int nb = 0;
+            while (i < s.size() && s[i] >= '0' && s[i] <= '9')
+            {
+                nb *= 10;
+                nb += s[i] - '0';  
+                i++;
+            }
+            if (s[i] == '#')
+            {
+                i++;
+                int j = 0;
+                while (j < nb)
+                {
+                    str.push_back(s[i]);
+                    i++;
+                    j++;
+                }
+            }
+            res.push_back(str);
+        }
+        return res;
+    }
+};
